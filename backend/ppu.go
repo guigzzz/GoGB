820e38c516f0f708162ae66bf27c814df7ff6faf
Guillaume Rame <guilrame@amazon.com>
2018-12-22 21:58:28 +0100

    started to think about background rendering

--- backend/ppu.go
+++ backend/ppu.go
@@ -5,86 +5,151 @@ import (
 	"image"
 )
 
+// Bit 7 - LCD Display Enable             (0=Off, 1=On)
+// Bit 6 - Window Tile Map Display Select (0=9800-9BFF, 1=9C00-9FFF)
+// Bit 5 - Window Display Enable          (0=Off, 1=On)
+// Bit 4 - BG & Window Tile Data Select   (0=8800-97FF, 1=8000-8FFF)
+// Bit 3 - BG Tile Map Display Select     (0=9800-9BFF, 1=9C00-9FFF)
+// Bit 2 - OBJ (Sprite) Size              (0=8x8, 1=8x16)
+// Bit 1 - OBJ (Sprite) Display Enable    (0=Off, 1=On)
+// Bit 0 - BG Display (for CGB see below) (0=Off, 1=On)
+
 // PPU represents the pixel processing unit
 // contains references to ram sections containing video relevant data
 type PPU struct {
-	// TilePatternT    []byte // Tile Pattern Table
-	// BackgroundTileT []byte // Background Tile Table
-	// WindowTileT     []byte // Window Tile Table
-	// OAM             []byte // Object Attribute Memory
-	ram        [1 << 16]byte   // reference to memory shared with CPU
-	image      *image.RGBA     // represents the current screen buffer
-	background [256 * 256]byte // contains the whole background
+	ram          [1 << 16]byte   // reference to memory shared with CPU
+	image        *image.RGBA     // represents the current screen
+	screenBuffer [144 * 160]byte // contains the pixels to draw on next refresh
 }
 
 // NewPPU creates a new PPU object
 func NewPPU(c CPU) PPU {
 	p := PPU{}
-	// p.TilePatternT = c.ram[0x8000:0x9800]    // 0x17FF bytes
-	// p.BackgroundTileT = c.ram[0x9800:0x9C00] // 32 * 32 bytes
-	// p.WindowTileT = c.ram[0x9C00:0xA000]     // 32 * 32 bytes
-	// p.OAM = c.ram[0xFE00:0xFEA0]             // 40 * 4 bytes
 	p.ram = c.ram
 	p.image = image.NewRGBA(image.Rectangle{image.Point{0, 0}, image.Point{160, 144}})
 	return p
 }
 
-func (p *PPU) DrawBackground() {
-	// lcdc := p.ram[0xFF40] // LCD Control Register
+const (
+	bgDisplay                  = iota // (0=Off, 1=On)
+	bgJDisplayEnable                  // (0=Off, 1=On)
+	objSize                           // (0=8x8, 1=8x16)
+	bgTileMapDisplaySelect            // (0=9800-9BFF, 1=9C00-9FFF)
+	bgWindowTileDataSelect            // (0=8800-97FF, 1=8000-8FFF)
+	windowDisplayEnable               // (0=Off, 1=On)
+	windowTileMapDisplaySelect        // (0=9800-9BFF, 1=9C00-9FFF)
+	lcdDisplayEnable                  // (0=Off, 1=On)
+)
 
-	// // Bit 7 - LCD Display Enable             (0=Off, 1=On)
-	// // Bit 6 - Window Tile Map Display Select (0=9800-9BFF, 1=9C00-9FFF)
-	// // Bit 5 - Window Display Enable          (0=Off, 1=On)
-	// // Bit 4 - BG & Window Tile Data Select   (0=8800-97FF, 1=8000-8FFF)
-	// // Bit 3 - BG Tile Map Display Select     (0=9800-9BFF, 1=9C00-9FFF)
-	// // Bit 2 - OBJ (Sprite) Size              (0=8x8, 1=8x16)
-	// // Bit 1 - OBJ (Sprite) Display Enable    (0=Off, 1=On)
-	// // Bit 0 - BG Display (for CGB see below) (0=Off, 1=On)
+func (p *PPU) lcdControlRegisterIsBitSet(bitnum uint) bool {
+	if bitnum > 7 {
+		panic(fmt.Sprintf("Got unexpected bit number %d higher than 7 (max for byte)", bitnum))
+	}
+	return p.ram[0xFF40]&(1<<bitnum) > 0
+}
 
-	// var mask byte = 1 << 3
-	// var bgTileMapSelect []byte
-	// if lcdc&mask == 0 {
-	// 	bgTileMapSelect = p.ram[0x9800 : 0x9BFF+1]
-	// } else {
-	// 	bgTileMapSelect = p.ram[0x9C00 : 0x9FFF+1]
-	// }
+func (p *PPU) getBackgroundTileData() ([]byte, bool) {
+	if p.lcdControlRegisterIsBitSet(bgWindowTileDataSelect) {
+		return p.ram[0x8000 : 0x8FFF+1], false
+	}
+	return p.ram[0x8800 : 0x97FF+1], true
+}
 
-	// mask = 1 << 4
-	// var bgTileMapData []byte
-	// var indexOffset int
-	// if lcdc&mask == 0 {
-	// 	// index will be signed -128 -> 127
-	// 	bgTileMapData = p.ram[0x8800 : 0x9BFF+1]
-	// 	indexOffset = -128
+func (p *PPU) getWindowTileData() ([]byte, bool) {
+	return p.getBackgroundTileData()
+}
 
-	// } else {
-	// 	// index will be unsigned 0 -> 255
-	// 	bgTileMapData = p.ram[0x8000 : 0x8FFF+1]
-	// 	indexOffset = 0
-	// }
+func (p *PPU) getBackgroundTileMap() []byte {
+	if p.lcdControlRegisterIsBitSet(bgTileMapDisplaySelect) {
+		return p.ram[0x9C00 : 0x9FFF+1]
+	}
+	return p.ram[0x9800 : 0x9BFF+1]
+}
 
-	// palette := p.ram[0xFF47]
+func (p *PPU) getWindowTileMap() []byte {
+	if p.lcdControlRegisterIsBitSet(windowTileMapDisplaySelect) {
+		return p.ram[0x9C00 : 0x9FFF+1]
+	}
+	return p.ram[0x9800 : 0x9BFF+1]
+}
 
-	// for i, b := range bgTileMapSelect {
-	// 	ind := int(b)
-	// 	tile := bgTileMapData[(indexOffset+ind)*16 : (indexOffset+ind+1)*16] // 16 bytes per tile
-	// }
+func mapColorToPalette(palette byte, color byte) byte {
+	return (palette >> (color * 2)) & 0x3
+}
+
+func (p *PPU) getBGPalette() byte {
+	return p.ram[0xFF47]
+}
+
+func (p *PPU) getScroll() (byte, byte) {
+	return p.ram[0xFF42], p.ram[0xFF43]
+}
+
+func (p *PPU) getBackgroundPixels(lineNumber byte) [144]byte {
+
+	tileMap := p.getBackgroundTileMap()
+	tileData, interpretIndexAsSigned := p.getBackgroundTileData()
+
+	scrollY, scrollX := p.getScroll()
+	rowInTile := scrollY % 8
+	tileRow := (scrollY + lineNumber) / 8
+	pixels := [144]byte{}
+
+	for i := byte(0); i < 144; i++ {
+
+		// compute in which background tile we fall in (in a 32 x 32 grid)
+		tileColumn := (scrollX + i) / 8
+		tileIndex := tileRow*32 + tileColumn
+
+		// get the tile data index for that tile
+		tileMapIndex := tileMap[tileIndex]
+
+		// if we are using 0x8000 to 0x8FFF
+		// then 0-127 maps to 8000-87FF and 128-255 maps to 8800-8FFF
+		//
+		// if we are using the 0x8800 to 0x97FF
+		// then 0-127 maps to 9000-97FF whereas 128-255 maps to 8800-8FFF
+		//
+		// we can just flip the MSB of the data index in the 0x8800 to 0x97FF case
+		if interpretIndexAsSigned {
+			tileMapIndex ^= 0x80
+		}
+
+		// 16 bytes per tile, 8 lines of 8 pixels per tiles
+		// meaning 2 bytes per line
+		lineDataIndex := tileMapIndex*16 + 2*rowInTile
+		lineData := tileData[lineDataIndex : lineDataIndex+2]
+
+		msb := lineData[1] >> (6 - i%8)
+		lsb := lineData[0] >> (7 - i%8)
+
+		pixels[i] = mapColorToPalette(p.getBGPalette(), msb|lsb)
+	}
+
+	return pixels
+}
+
+func (p *PPU) getWindowPixels() {
 
 }
 
-func (p *PPU) DrawLine() {
+func (p *PPU) getSpritePixels() {
 
-	// fetch relevant memory
-	// TilePatternT := p.ram[0x8000:0x9800]    // 0x17FF bytes
-	// BackgroundTileT := p.ram[0x9800:0x9C00] // 32 * 32 bytes
-	// WindowTileT := p.ram[0x9C00:0xA000]     // 32 * 32 bytes
-	// OAM := p.ram[0xFE00:0xFEA0]             // 40 * 4 bytes
+}
 
-	// ScrollY := p.ram[0xFF42]
-	// ScrollX := p.ram[0xFF43]
+func (p *PPU) drawLine(lineNumbers chan byte) {
+	// lineNumber will vary from 0 to 153
+	// 0 to 143 included is to draw each one of the 144 screen lines
+	// 144 to 153 is V-blank period
 
-	fmt.Println(len(p.image.Pix) / 4)
+	for lineNumber := range lineNumbers {
+		pixels := p.getBackgroundPixels(lineNumber)
+		// window pixels := getWindowPixels
+		// spritePixels := getSpritePixels
 
-	// p.image
+		for i, pixel := range pixels {
+			p.screenBuffer[int(lineNumber)*160+i] = pixel
+		}
+	}
 
 }
