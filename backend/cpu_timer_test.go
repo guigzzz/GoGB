4005a18431451704820d46379d6e6b29c394dd40
Guillaume <guillaume.ws.rame@gmail.com>
2021-12-26 00:37:57 +0100

    add timer unit tests

---
+++ backend/cpu_timer_test.go
@@ -0,0 +1,61 @@
+package backend
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimerFiresWhenItShould(t *testing.T) {
+
+	c := NewTestCPU()
+
+	c.cycleCounter = 1020
+
+	tma := byte(0x5)
+
+	c.ram[0xFF07] = 0b100 // TAC
+	c.ram[0xFF06] = tma   // TMA
+	c.ram[0xFF05] = 0xFF  // TIMA
+
+	c.checkForTimerIncrementAndInterrupt(10)
+
+	assert.Equal(t, tma, c.ram[0xFF05])
+	assert.Equal(t, byte(0x4), c.ram[0xFF0F])
+}
+
+func TestTimerDoesNotFireWhenTimerOff(t *testing.T) {
+
+	c := NewTestCPU()
+
+	c.cycleCounter = 1020
+
+	tma := byte(0x5)
+
+	c.ram[0xFF07] = 0b0  // TAC
+	c.ram[0xFF06] = tma  // TMA
+	c.ram[0xFF05] = 0xFF // TIMA
+
+	c.checkForTimerIncrementAndInterrupt(10)
+
+	assert.Equal(t, byte(0xFF), c.ram[0xFF05])
+	assert.Equal(t, byte(0), c.ram[0xFF0F])
+}
+
+func TestTimerDoesNotFireWhenItShouldNot(t *testing.T) {
+
+	c := NewTestCPU()
+
+	c.cycleCounter = 1010
+
+	tma := byte(0x5)
+
+	c.ram[0xFF07] = 0b100 // TAC
+	c.ram[0xFF06] = tma   // TMA
+	c.ram[0xFF05] = 0xFF  // TIMA
+
+	c.checkForTimerIncrementAndInterrupt(10)
+
+	assert.Equal(t, byte(0xFF), c.ram[0xFF05])
+	assert.Equal(t, byte(0), c.ram[0xFF0F])
+}
